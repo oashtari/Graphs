77c7ced9d756bdde7b480f334be8c7d5d3f9bf64
Omid Ashtari <oashtari@gmail.com>
2020-11-22 07:33:46 -0800

    funtioning dfs, it seems

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -4,6 +4,9 @@ from world import World
 
 import random
 from ast import literal_eval
+from util import Queue, Stack
+
+import multiprocessing
 
 # Load world
 world = World()
@@ -11,9 +14,9 @@ world = World()
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
-map_file = "maps/test_loop_fork.txt"
+# map_file = "maps/test_loop_fork.txt"
 # map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
@@ -27,22 +30,71 @@ player = Player(world.starting_room)
 
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
+traversal_path = []
 
-def try_traversal():
+def generate_path():
 
     graph = {}
-    
+
     visited = set()
 
-    traversal_path = []
+    the_path = []
+
+    def dft(room):
+        current_room = room
+        previous = None
+
+        while len(graph.keys()) < len(room_graph):
+            exits = current_room.get_exits()
+            # print('pre add visited', visited)
+
+            if current_room.id not in visited:
+                print('FRESH ID', current_room.id)
+                for exit in exits:
+                    # print('exit', exit)
+                    print('initial graph', graph)
+                    if current_room.id in graph:
+                        graph[current_room.id][exit] = None
+                        print('yes in graph', graph)
+                    else:
+                        graph[current_room.id] = {exit: None}
+                        print('NOT in graph', graph)
+
+
+            visited.add(current_room.id)
+            # print('post add visited', visited)
+
+            if previous:
+                if previous[0] == "n":
+                    graph[current_room.id]["s"] = previous[1]
+                if previous[0] == "s":
+                    graph[current_room.id]["n"] = previous[1]
+                if previous[0] == "e":
+                    graph[current_room.id]["w"] = previous[1]
+                if previous[0] == "w":
+                    graph[current_room.id]["e"] = previous[1]
+            print('previous', previous)
+
+            not_attempted = [exit for exit in exits if graph[current_room.id][exit] is None]
+            print('not attempted', not_attempted)
+
+            if len(not_attempted) == 0:
+                print('CURRENT', current_room)
+                return current_room
+
+            direction = random.choice(not_attempted)
+            print('new direction', direction)
+
+            the_path.append(direction)
+            print('THE path', the_path)
+            new_room = current_room.get_room_in_direction(direction)
+            graph[current_room.id][direction] = new_room.id
+            previous = [direction, current_room.id]
+            current_room = new_room
 
 
 
-
-
-
-
-# TRAVERSAL TEST
+# TRAVERSAL TEST - DO NOT MODIFY
 visited_rooms = set()
 player.current_room = world.starting_room
 visited_rooms.add(player.current_room)
@@ -52,13 +104,20 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
 
 
 
+
+
+
+
+# if __name__ == "__main__":
+#     test_traversal()
 #######
 # UNCOMMENT TO WALK AROUND
 #######
@@ -71,3 +130,5 @@ else:
 #         break
 #     else:
 #         print("I did not understand that command.")
+
+
