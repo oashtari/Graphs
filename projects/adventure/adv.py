00813161477d6f12580cdebab61465b15ae6cb4b
Omid Ashtari <oashtari@gmail.com>
2020-11-22 18:12:46 -0800

    finished bfs

--- projects/adventure/adv.py
+++ projects/adventure/adv.py
@@ -52,13 +52,13 @@ def generate_path():
                 print('FRESH ID', current_room.id)
                 for exit in exits:
                     # print('exit', exit)
-                    print('initial graph', graph)
+                    # print('initial graph', graph)
                     if current_room.id in graph:
                         graph[current_room.id][exit] = None
-                        print('yes in graph', graph)
+                        # print('yes in graph', graph)
                     else:
                         graph[current_room.id] = {exit: None}
-                        print('NOT in graph', graph)
+                        # print('NOT in graph', graph)
 
 
             visited.add(current_room.id)
@@ -76,21 +76,66 @@ def generate_path():
             print('previous', previous)
 
             not_attempted = [exit for exit in exits if graph[current_room.id][exit] is None]
-            print('not attempted', not_attempted)
+            # print('not attempted', not_attempted)
 
             if len(not_attempted) == 0:
-                print('CURRENT', current_room)
+                # print('CURRENT', current_room)
                 return current_room
 
             direction = random.choice(not_attempted)
-            print('new direction', direction)
+            # print('new direction', direction)
 
             the_path.append(direction)
-            print('THE path', the_path)
+            # print('THE path', the_path)
             new_room = current_room.get_room_in_direction(direction)
             graph[current_room.id][direction] = new_room.id
             previous = [direction, current_room.id]
             current_room = new_room
+
+    def bfs(starting_room):
+        paths_dict = {}
+        todo = []
+        completed = set()
+
+        todo.append([starting_room])
+        while len(todo) > 0 and len(graph.keys()) < len(room_graph):
+            rooms = todo.pop(0)
+            current_room = rooms[-1]
+            print('current bfs room', current_room)
+
+            if current_room.id not in completed:
+                if None in graph[current_room.id].values():
+                    the_path.extend(paths_dict[current_room.id])
+                    return current_room
+                completed.add(current_room.id)
+                print('completed bfs', completed)
+
+                exits = current_room.get_exits()
+
+                for exit in exits:
+                    next_room = current_room.get_room_in_direction(exit)
+                    if current_room.id in paths_dict:
+                        next_room_path = list(paths_dict[current_room.id])
+                        next_room_path.append(exit)
+                        paths_dict[next_room.id] = next_room_path
+                    else:
+                        paths_dict[next_room.id] = [exit]
+                    new_rooms = list(rooms)
+                    new_rooms.append(next_room)
+                    todo.append(new_rooms)
+                    print('todo bfs', todo)
+
+    current_room = player.current_room
+
+    while len(graph.keys()) < len(room_graph):
+        dft_last_room = dft(current_room)
+
+        bfs_last_room = bfs(dft_last_room)
+
+        current_room = bfs_last_room
+
+    print('THE PATH', the_path)
+    return the_path
 
 
 
@@ -109,10 +154,6 @@ if len(visited_rooms) == len(room_graph):
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
-
-
-
 
 
 
